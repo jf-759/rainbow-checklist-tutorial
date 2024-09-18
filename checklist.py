bf25224433db9906aec4c0ee71a694f20caa529d
jf-759 <jane.fernandez@students.dominican.edu>
2024-09-18 16:55:34 -0700

    created helper functions

--- checklist.py
+++ checklist.py
@@ -29,6 +29,36 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(str(index) + list_item)
+        index += 1
+
+def mark_completed(index):
+    checklist[index] = ("{} {}" .format("√", checklist[index]))
+
+def select(function_code):
+   
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+
+        read(item_index)
+
+    elif function_code == "P":
+        list_all_items()
+
+    else:
+        print("Unknown Option")
+
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
 def test():
     
     create("purple sox")
@@ -38,9 +68,22 @@ def test():
     print(read(1))
 
     update(0, "purple socks")
+    
     destroy(1)
 
     print(read(0))
     # print(read(1))
+    
+    select("C")
 
+    list_all_items()
+
+    select("R")
+
+    list_all_items()
+
+    user_value = user_input("Please Enter a value:")
+    int(user_value)
+    print(user_value)
+ 
 test()
