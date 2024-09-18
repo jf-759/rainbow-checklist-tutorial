d0682f111b22d07668d1bb704e8c968cdf799839
jf-759 <jane.fernandez@students.dominican.edu>
2024-09-18 15:57:53 -0700

    created CRUD functions

--- checklist.py
+++ checklist.py
@@ -1,2 +1,46 @@
-print("Hello World")
 checklist = list()
+
+# def create(item):
+#     checklist.append('Blue')
+
+def create(item):
+    checklist.append(item)
+
+# checklist[0]
+
+# def read(index):
+#     item = checklist[index]
+#     return item
+
+def read(index):
+    return checklist[index]
+
+# checklist = ['Blue', 'Orange']
+# checklist [1] = 'Cats'
+# print(checklist)
+
+def update(index, item):
+    checklist [index] = item
+
+# checklist = ['Blue', 'Cats']
+# checklist.pop(1)
+# print(checklist)
+
+def destroy(index):
+    checklist.pop(index)
+
+def test():
+    
+    create("purple sox")
+    create ("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    # print(read(1))
+
+test()
